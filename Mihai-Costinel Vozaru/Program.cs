70bf3f4496abd1f9c4fe549feb040074a1aa8115
VozaruMihai <92035346+VozaruMihai@users.noreply.github.com>
2021-11-14 22:58:34 +0200

    Add files via upload

---
+++ Mihai-Costinel Vozaru/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace L05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var count = 0;
+            while (true){
+                Console.WriteLine("Cycle no. " + count.ToString());
+                Console.WriteLine("Date: " + DateTime.Now.ToLocalTime());
+                count++;
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
